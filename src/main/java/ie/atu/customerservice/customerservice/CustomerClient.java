1c34a3d7e1bbd9e57838c64c28085f03aa816fb0
conor-keane <g00391846@atu.ie>
2023-11-08 10:03:37 +0000

    Added GetMapping to customer controller

--- src/main/java/ie/atu/customerservice/customerservice/CustomerClient.java
+++ src/main/java/ie/atu/customerservice/customerservice/CustomerClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "Customer-service", url = "http://localhost:8082")
-public class CustomerClient {
+public interface CustomerClient {
     @GetMapping("/customerId")
     CustomerDetails getCustomerById(@PathVariable("customerId") int customerId);
 
--- src/main/java/ie/atu/customerservice/customerservice/CustomerController.java
+++ src/main/java/ie/atu/customerservice/customerservice/CustomerController.java
@@ -1,4 +1,20 @@
 package ie.atu.customerservice.customerservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public CustomerDetails getCustomerById(@PathVariable int customerId){
+        return customerService.getCustomerById(customerId);
+    }
+
 }
